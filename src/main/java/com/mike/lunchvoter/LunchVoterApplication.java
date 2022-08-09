a00458ccf18c08bf6e3b7660cda4a361e1be819a
Mikka <gfhjkm54>
2022-08-09 22:05:03 +0700

    Initial spring boot project

---
+++ src/main/java/com/mike/lunchvoter/LunchVoterApplication.java
@@ -0,0 +1,13 @@
+package com.mike.lunchvoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchVoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchVoterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mike/lunchvoter/LunchVoterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.lunchvoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchVoterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
